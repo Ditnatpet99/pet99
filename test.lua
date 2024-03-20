1b1b6117f01a055f3c55a0cc6855d41ba72fefdd
Ditnatpet99 <164035241+Ditnatpet99@users.noreply.github.com>
2024-03-20 13:05:52 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,4 +1,22 @@
+local namefile = game.Players.LocalPlayer.Name .. "-Pet99.json"
 
+local HttpService = game:GetService("HttpService")
+
+local function Load()
+    if isfile(namefile) then
+        local fileContents = readfile(namefile)
+        if fileContents then
+            local decodedConfig = HttpService:JSONDecode(fileContents)
+            if decodedConfig then
+                for key, value in pairs(decodedConfig) do
+                    Config[key] = value
+                end
+            end
+        end
+    else
+        writefile(namefile, HttpService:JSONEncode(Config))
+    end
+end
 local iditem = nil
 local save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
 for i, v in pairs(save.Get().Inventory.Currency) do
@@ -8,18 +26,16 @@ for i, v in pairs(save.Get().Inventory.Currency) do
 end
 spawn(function()
     while wait() do
-        if Config["Value"] ~= nil then
-            if game:GetService("Players").LocalPlayer.leaderstats["\240\159\146\142 Diamonds"].Value >= Config["Value"] then
-                local args = {
-                    [1] = Config["Account"],
-                    [2] = "Made By Honglamx",
-                    [3] = "Currency",
-                    [4] = tostring(iditem),
-                    [5] = Config["Value"]
-                }
-                game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(
-                    unpack(args))
-            end
+        if game:GetService("Players").LocalPlayer.leaderstats["\240\159\146\142 Diamonds"].Value >= Config["Value"] then
+            local args = {
+                [1] = Config["Account"],
+                [2] = "Made By Honglamx",
+                [3] = "Currency",
+                [4] = tostring(iditem),
+                [5] = Config["Value"]
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(
+                unpack(args))
         end
     end
 end)
